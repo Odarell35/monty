d4dd66d4da21456cbd2bd2099cf3c781afdcd262
Odarell35 <leratomawash0@gmail.com>
2023-06-20 03:20:55 -0700

    prints stack values

---
+++ pall.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * pall_data -  prints all the values on the stack,
+ * starting from the top of the stack.
+ * @stack: stack head
+ * @line_number: no used
+ * Return: no return
+*/
+void pall_data(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	(void) line_number;
+
+	temp = *stack;
+	while(temp)
+	{
+		printf("%d\n",temp->n);
+		temp = temp->next;
+	}
+}	
